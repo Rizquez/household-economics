de647286bb3745e78276b0f5b6f5583692426663
Pedro Rizquez <pedro.rizquez.94@gmail.com>
2026-07-11 16:25:48 +0200

    fix: DB - typed

--- database/services/family.py
+++ database/services/family.py
@@ -1,11 +1,16 @@
-from typing import Dict
+from __future__ import annotations
+
+from typing import Dict, TYPE_CHECKING
 
 from models import Family
 from .core import ServiceBase
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import scoped_session
 
 
 class FamilyService(ServiceBase):
 
     @classmethod
-    def create_family(cls, session, a_dict: Dict) -> Family:
+    def create_family(cls, session: "scoped_session", a_dict: Dict) -> Family:
         return cls.create(session, a_dict, Family)
--- database/services/role.py
+++ database/services/role.py
@@ -1,11 +1,16 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
 
 from models import Role
 from .core import ServiceBase
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import scoped_session
 
 
 class RoleService(ServiceBase):
 
     @classmethod
-    def get_role_by_name(cls, session, name: str) -> Optional[Role]:
+    def get_role_by_name(cls, session: "scoped_session", name: str) -> Optional[Role]:
         return cls.find(session, Role.name == name, model=Role)
