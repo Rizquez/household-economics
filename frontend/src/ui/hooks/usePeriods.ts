47985ffa7d56d8cfc343ab55188a9e996b98bbfb
sr-rizquez <SR.RIZQUEZ.96@GMAIL.COM>
2026-07-23 21:42:51 +0200

    fix: just format

--- frontend/src/ui/hooks/usePeriods.ts
+++ frontend/src/ui/hooks/usePeriods.ts
@@ -1,17 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMonthlyTrackingPeriods } from "@/core/business/monthly-tracking/services";
-import { SAVINGS_INVESTMENTS_PERIODS_QUERY_KEY, SAVINGS_INVESTMENTS_QUERY_KEY } from "../pages/SavingsInvestments/hooks/constants";
-import { MONTHLY_TRACKING_QUERY_KEY, PERIODS_QUERY_KEY } from "../pages/MonthlyTracking/hooks/constants";
+import {
+  SAVINGS_INVESTMENTS_PERIODS_QUERY_KEY,
+  SAVINGS_INVESTMENTS_QUERY_KEY,
+} from "../pages/SavingsInvestments/hooks/constants";
+import {
+  MONTHLY_TRACKING_QUERY_KEY,
+  PERIODS_QUERY_KEY,
+} from "../pages/MonthlyTracking/hooks/constants";
 import { DASHBOARD_PERIODS_QUERY_KEY } from "../pages/Dashboard/hooks/constants";
 
 const usePeriods = () => {
   const query = useQuery({
     queryKey: [
-        SAVINGS_INVESTMENTS_PERIODS_QUERY_KEY,
-        SAVINGS_INVESTMENTS_QUERY_KEY,
-        MONTHLY_TRACKING_QUERY_KEY, 
-        PERIODS_QUERY_KEY,
-        DASHBOARD_PERIODS_QUERY_KEY],
+      SAVINGS_INVESTMENTS_PERIODS_QUERY_KEY,
+      SAVINGS_INVESTMENTS_QUERY_KEY,
+      MONTHLY_TRACKING_QUERY_KEY,
+      PERIODS_QUERY_KEY,
+      DASHBOARD_PERIODS_QUERY_KEY,
+    ],
     queryFn: () => getMonthlyTrackingPeriods.execute(),
   });
   return {
